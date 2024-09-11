231ff4243ce452012057409345d2af0b6f5ad5aa
Ethan-Tsai <ethan-tsai-0129@outlook.com>
2024-09-11 15:53:16 +0800

    Update DBconfig.js

--- APP/Client/screens/config/DBconfig.js
+++ APP/Client/screens/config/DBconfig.js
@@ -1,10 +1,8 @@
+const API = 'http://140.117.71.127:3000'; // API 在專案教室
 
-// const API = 'http://172.20.53.100:3000'; // API 在專案教室
 
-const API = 'http://140.117.71.127:3000';
-/*
-const API = 'http://140.117.71.183:3000'; // API 在老師電腦
-*/
+// const API = 'http://140.117.71.183:3000'; // API 在老師電腦
+
 
 
 module.exports = API;
