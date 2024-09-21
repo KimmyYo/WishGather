34581cbbdb91108cb21cbd1cdb5be3a379f96737
KimmYo <113627877+KimmyYo@users.noreply.github.com>
2024-09-21 23:08:55 +0800

    新增編輯法會頁面修改與資料庫連接

--- APP/Client/screens/Temple/EditTempleInfoPage.js
+++ APP/Client/screens/Temple/EditTempleInfoPage.js
@@ -4,25 +4,92 @@ import { SafeAreaProvider,  useSafeAreaInsets } from 'react-native-safe-area-con
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { UserContext } from '../../components/Context/UserContext';
 import  Dialog  from "react-native-dialog";
-import Lunar from '@tony801015/chinese-lunar';
+import { formatDate, convertSolarDateToLunarDate } from '../../components/Utility/DateUtils';
+import axios from 'axios';
 
 import TextInputSet from '../../components/Utility/TextInputSet';
-
 import PageTitle from '../../components/Utility/PageTitle';
 import SetButton from '../../components/Utility/SetButton';
 import DatePicker from '../../components/Utility/DatePicker';
 import NavigateBack from '../../components/Utility/NavigateBack';
 
+const base = require('../config/DBconfig');
+const insertEventApi = `${base}/temple_event`;
+
 function EditTempleInfoPage(){
     const navigation = useNavigation();
     const route = useRoute();
+    const [dialogVisible, setDialogVisible] = useState(false);
+    const [alertDialogVisible, setAlertDialogVisible] = useState(false);
     // 編輯頁傳入的資料
     const event = route.params.event;
     const forEdit = route.params.forEdit;
     const { userId } = useContext(UserContext);
+   
     const insets = useSafeAreaInsets();
-    const [date, setDate] = useState(new Date(event.DATE));
+    const [selectedDate, setSelectedDate] = useState(event.DATE ? new Date(event.DATE) : new Date());
+    const [eventDesc, setEventDesc] = useState(event.DESCRIPTION || '');
+    const [eventName, setEventName] = useState(event.NAME || '');
 
+    const insertTempleEvent = () => {
+       // Ensure all necessary data is defined
+        if (!userId || !eventName || !eventDesc || !selectedDate) {
+            console.error('請確保所有必要的事件資訊都已填寫');
+            return;
+        }
+        const eventData = {
+            tID: userId,
+            eID: event.eID,
+            NAME: eventName,
+            DESCRIPTION: eventDesc,
+            DATE: new Date(selectedDate).toISOString().split('T')[0],
+        };
+        console.log(eventData);
+
+        axios.post(insertEventApi, eventData)
+            .then((response) => {
+                if (response.status === 200) {
+                    setAlertDialogVisible(true);
+                }
+            })
+            .catch((error) => {
+                console.error('儲存失敗:', error);
+            });
+    }
+    const handleConfirmKeepAdding = () => {
+        setEventDesc('');
+        setEventName('');
+        setSelectedDate(new Date());
+        setTimeout(() => {
+            setDialogVisible(false); // Show the second dialog after a delay
+        }, 10); 
+        console.log(selectedDate, eventDesc, eventName);
+    }
+    const handleAddSucceedAlert = () => {
+        setAlertDialogVisible(false); // Close the first dialog
+    };
+    
+    const renderConfirmDialog = () => {
+        return (
+            <Dialog.Container visible={dialogVisible}>
+                <Dialog.Title>是否繼續新增法會？</Dialog.Title>
+                <Dialog.Description>
+                </Dialog.Description>
+                <Dialog.Button label="取消新增" onPress={() => navigation.navigate('TempleEventPage', { refresh: true })}/>
+                <Dialog.Button label="繼續新增" onPress={handleConfirmKeepAdding}/>
+            </Dialog.Container>
+        )
+    }
+    const renderAlertDialog = () => {
+        return (
+            <Dialog.Container visible={alertDialogVisible}>
+                <Dialog.Title>儲存成功!</Dialog.Title>
+                <Dialog.Description>
+                </Dialog.Description>
+                <Dialog.Button label="確定" onPress={handleAddSucceedAlert}/>
+            </Dialog.Container>
+        )
+    }
     return (
         <SafeAreaProvider>
             <View
@@ -37,10 +104,10 @@ function EditTempleInfoPage(){
                 }}
             >
                 <View style={styles.buttonContainer}>
-                    <Pressable onPress={() => navigation.goBack()}>
+                    <Pressable onPress={() => navigation.navigate('TempleEventPage', { refresh: true })}>
                         <Text style={styles.textDanger}>取消</Text>
                     </Pressable>
-                    <Pressable onPress={() => navigation.goBack()}>
+                    <Pressable onPress={() => setDialogVisible(true)}>
                         <Text style={styles.textSucceed}>完成</Text>
                     </Pressable>
                 </View>
@@ -48,20 +115,34 @@ function EditTempleInfoPage(){
                 <PageTitle  iconName ={forEdit ? "edit" : "add"} titleText={forEdit ? "編輯法會資訊" : "新增法會資訊"}></PageTitle>
                 
                 <View style={styles.formContainer}>
-                    <TextInputSet labelName={'法會名稱'} defaultValue={event.NAME} multiLine={false} placeholder={"請輸入法會名稱"}/>
-                    <DatePicker dateValue={event.DATE} labelName={"農曆日期"} isLunar={true} editable={false} /> 
-                    <DatePicker dateValue={event.DATE} labelName={"國曆日期"} isLunar={false} editable={true}/>
-                    <TextInputSet labelName={'法會簡介'} defaultValue={event.DESCRIPTION} multiLine={true} placeholder={"請輸入法會簡介"}/>
+                    <TextInputSet 
+                        labelName={'法會名稱'} 
+                        defaultValue={eventName} 
+                        multiLine={false} 
+                        placeholder={"請輸入法會名稱"}
+                        onChange={(newName) => setEventName(newName)}/>
+                    <DatePicker 
+                        dateValue={selectedDate} 
+                        isLunar={false} 
+                        editable={true}
+                        onChange={(newSolarDate) => setSelectedDate(newSolarDate)}
+                    />
+                    <TextInputSet 
+                        labelName={'法會簡介'} 
+                        defaultValue={eventDesc} 
+                        multiLine={true} 
+                        placeholder={"請輸入法會簡介"}
+                        onChange={(newDescription) => setEventDesc(newDescription)}
+                    />
                 </View>
-                              
-                {/* <View style={styles.buttonPosition}>
-                    <SetButton 
-                        btnText={'確定'} 
-                        btnStatus={'primary'}
-                        navigateScreen={'TempleEventPage'}
-                    >
-                    </SetButton>
-                </View> */}
+                <SetButton
+                    btnText = '確定'
+                    btnStatus = 'primary'
+                    onPress={insertTempleEvent}
+                />
+                {renderConfirmDialog()}      
+                {renderAlertDialog()}
+                
             </View>
         </SafeAreaProvider>
     )
